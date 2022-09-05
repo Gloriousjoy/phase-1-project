817f3714405a65e71e60e3a849912c62c6bc8b48
Gloriousjoy <gloriousfab99@gmail.com>
2022-09-05 13:30:36 +0300

    https

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const loadFact = ()=>{
     document.getElementById("sendNumber").addEventListener("click", async(e)=>{
          e.preventDefault
          const inputValue = document.querySelector("#numbers").value
-         const fetchNumbers =  await fetch (`http://numbersapi.com/${inputValue}`)
+         const fetchNumbers =  await fetch (`https://numbersapi.com/${inputValue}`)
          const fetchedNumbers = await fetchNumbers.text()
          document.querySelector("#numbers").value
          document.getElementById("receive-number").innerText= fetchedNumbers
